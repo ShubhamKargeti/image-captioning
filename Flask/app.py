fe5052e78fdba43b16a4414add84407dc2323dd6
Rahul Singh <52593278+rahulsingh731@users.noreply.github.com>
2020-12-07 18:30:58 +0530

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from flask import render_template, request
+import os
 
+app = Flask(__name__)
+upload_folder = "/home/rahul/Self_ML_try/image_captioning/static"
+if not os.path.exists(upload_folder):
+    os.mkdir(upload_folder)
+@app.route("/", methods= ["GET", "POST"])
+def upload_predict():
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            image_location = os.path.join(upload_folder,image_file.filename)
+            image_file.save(image_location)
+    return render_template("index.html",prediction=1)
+
+if __name__=='__main__':
+    app.run(port=1200,debug=True)
+    
